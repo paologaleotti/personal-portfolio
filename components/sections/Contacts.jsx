d77bbc06ec764a022fd51b63f16157697aa1cff8
paologaleotti <paologaleotti2002@gmail.com>
2022-06-27 12:44:46 +0200

    updated image

--- components/sections/Contacts.jsx
+++ components/sections/Contacts.jsx
@@ -1,4 +1,4 @@
-import { Flex, Image, Stack } from "@chakra-ui/react";
+import { Flex, Image, Stack, Show } from "@chakra-ui/react";
 
 import ContactHeader from "../ui/contacts/ContactHeader";
 import ContactDesc from "../ui/contacts/ContactDesc";
@@ -7,16 +7,20 @@ import ContactButtons from "../ui/contacts/ContactButtons";
 function Landing(props) {
 	return (
 		<Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
-			<Flex flex={0.7}>
-				<Image
-					alt={"Login Image"}
-					objectFit={"cover"}
-					src={
-						"https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1925&q=80"
-					}
-					opacity="50%"
-				/>
-			</Flex>
+			<Show above="md">
+				<Flex flex={0.7}>
+					<Image
+						alt={"Login Image"}
+						objectFit={"cover"}
+						h="100vh"
+						src={
+							"https://images.unsplash.com/photo-1572890017012-5cd15e03c670?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"
+						}
+						opacity="50%"
+					/>
+				</Flex>
+			</Show>
+
 			<Flex p={8} flex={1} align={"center"} justify={"center"}>
 				<Stack spacing={6} w={"full"} maxW={"lg"}>
 					<ContactHeader />
